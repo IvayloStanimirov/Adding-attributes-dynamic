dcfad5c2e680815a68de7f5f5306b09064673b20
Ivaylo <kolevivaylo27@gmail.com>
2021-10-11 20:34:50 +0300

    Adding attributes.

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,9 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
+  let product = document.getElementsByClassName('product')[0]
+  let price = document.getElementsByClassName('price')[0].innerHTML
+
+  product.setAttribute('data-price', price)
 
 });
